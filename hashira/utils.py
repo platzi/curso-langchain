e5a858141701e22d681af6667d4372d4c7dbeccf
Omar U. Espejel <espejelomar@gmail.com>
2023-06-28 15:59:42 -0600

    funciones nuevas en utils

--- hashira/utils.py
+++ hashira/utils.py
@@ -107,3 +107,25 @@ def get_query_from_user() -> str:
     except EOFError:
         print("Error: Input no esperado. Por favor intenta de nuevo.")
         return get_query_from_user()
+
+
+def create_dir(path: str) -> None:
+    """
+    Crea un directorio si no existe.
+
+    Args:
+        path (str): Ruta del directorio a crear.
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def remove_existing_file(file_path: str) -> None:
+    """
+    Elimina un archivo si existe.
+
+    Args:
+        file_path (str): Ruta del archivo a eliminar.
+    """
+    if os.path.exists(file_path):
+        os.remove(file_path)
